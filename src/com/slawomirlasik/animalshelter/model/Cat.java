ccf3cf0ff0ed7f25ae1a9c7b7e3f51f3997bfa9b
Sławomir Lasik <slawomir.lasik@gmail.com>
2020-03-29 17:41:49 +0200

    added Animal Class to  a project with getters setters and Constructors

---
+++ src/com/slawomirlasik/animalshelter/model/Cat.java
@@ -0,0 +1,59 @@
+package com.slawomirlasik.animalshelter.model;
+
+import java.time.LocalDate;
+
+public class Cat {
+
+    private String name;
+    private String species;
+
+    private Float weight;
+
+    private LocalDate birthDate;
+    private LocalDate admissionDate;
+
+    public Cat(String name, String species, LocalDate birthDate, Float weight) {
+      this(name, species, birthDate, weight, LocalDate.now());
+    }
+
+    public Cat(String name, String species, LocalDate birthDate, Float weight, LocalDate admissionDate) {
+        this.name = name;
+        this.species = species;
+        this.birthDate = birthDate;
+        this.weight = weight;
+        this.admissionDate = admissionDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public LocalDate getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getAge(){
+        return LocalDate.now().getYear() - this.birthDate.getYear();
+    }
+}
