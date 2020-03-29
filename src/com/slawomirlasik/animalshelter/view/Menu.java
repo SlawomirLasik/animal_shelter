4cd89c35cdabe2f14b5d6d9b390ae37b7c9a9478
Sławomir Lasik <slawomir.lasik@gmail.com>
2020-03-29 17:51:09 +0200

    added message to Menu that you can add new animal

--- src/com/slawomirlasik/animalshelter/view/Menu.java
+++ src/com/slawomirlasik/animalshelter/view/Menu.java
@@ -3,6 +3,7 @@ package com.slawomirlasik.animalshelter.view;
 public class Menu {
 
     private final String STARTING_MESSAGE_INFO = "Welcome to AnimalShelter management system. \nWhat do you want to do?";
+    private final String ADDING_NEW_ANIMAL_MESSAGE = "Add new animal";
 
     public Menu() {
 
@@ -10,5 +11,6 @@ public class Menu {
 
     public void showMenu() {
         System.out.printf("%s", STARTING_MESSAGE_INFO);;
+        System.out.printf("1 - %s", ADDING_NEW_ANIMAL_MESSAGE);
     }
 }
