d804b2636ded5a677688291b6d6fd02d7d7ec920
Sławomir Lasik <slawomir.lasik@gmail.com>
2020-03-29 18:15:28 +0200

    Added container for Key -> value pair for storing all extensions in the ssystem wheere key is type of class stored in extension, and value is extension with class types

--- src/com/slawomirlasik/animalshelter/model/ExtensionClass.java
+++ src/com/slawomirlasik/animalshelter/model/ExtensionClass.java
@@ -1,6 +1,12 @@
 package com.slawomirlasik.animalshelter.model;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class ExtensionClass {
+
+    private Map<Class, HashSet<Class>> extensionMap = new HashMap<>();
 
 
 }
