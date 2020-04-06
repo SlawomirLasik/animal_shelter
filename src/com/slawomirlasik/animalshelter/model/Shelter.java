6fb8f1840bb250c6cd38f270bf3f6536d1d039b3
Sławomir Lasik <slawomir.lasik@gmail.com>
2020-04-06 22:18:41 +0200

    Added getNumberAnimalsInTheShelter method in Shelter class

--- src/com/slawomirlasik/animalshelter/model/Shelter.java
+++ src/com/slawomirlasik/animalshelter/model/Shelter.java
@@ -58,4 +58,9 @@ public class Shelter extends ExtensionClass {
         this.maximumCapacityOfAnimalsInShelter = maximumCapacityOfAnimalsInShelter;
     }
 
+    public Integer getNumberAnimalsInTheShelter() {
+        return getExtension(Animal.class).size();
+    }
+
+
 }
