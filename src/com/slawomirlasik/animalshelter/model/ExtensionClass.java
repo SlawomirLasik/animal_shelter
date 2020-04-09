13687f9555d1d260a5b866ca84e224b40b285637
Sławomir Lasik <slawomir.lasik@gmail.com>
2020-04-09 19:12:32 +0200

    Added loading previous status of Animal shelter functionality. Rewritten ExtensionClass Constructor for reading previous system status from a file to extension Map if possible, if not creating new emptu extension Map

--- src/com/slawomirlasik/animalshelter/model/ExtensionClass.java
+++ src/com/slawomirlasik/animalshelter/model/ExtensionClass.java
@@ -7,28 +7,43 @@ import java.util.Map;
 
 public class ExtensionClass implements Serializable {
 
-    private static Map<Class, HashSet<Class>> extensionMap = new Hashtable<>();
+    private static Map<Class, HashSet<Class>> extensionMap;
     private static final String DEFAULT_FILE_PATH = "data.adf";
 
 
     public ExtensionClass() {
-        Class extensionClassKey = this.getClass();
-        HashSet extention = null;
+        // load extension map from a file
+        try {
+            if (loadExtensionsFromFile()) {
+                System.out.println("Loaded previous Shelter status successfully");
+            } else {
+                System.out.println("Loading previous Shelter status failed. Creating empty lists of animals...");
+                Class extensionClassKey = this.getClass();
+                HashSet extention = null;
 
-        if(extensionMap.containsKey(extensionClassKey)){
-            // we have extension with this key and we retrieve it
-            extention = extensionMap.get(extensionClassKey);
-        }else{
-            // we do not have this extension key and we have to create it
-            extention = new HashSet<>();
-            extensionMap.put(extensionClassKey, extention);
+                extensionMap = new Hashtable<>();
+                if (extensionMap.containsKey(extensionClassKey)) {
+                    // we have extension with this key and we retrieve it
+                    extention = extensionMap.get(extensionClassKey);
+                } else {
+                    // we do not have this extension key and we have to create it
+                    extention = new HashSet<>();
+                    extensionMap.put(extensionClassKey, extention);
+                }
+                // we add current class to the extension
+                extention.add(this);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-        // we add current class to the extension
-        extention.add(this);
+
+
     }
 
     // add method of getting extension (animal extension)
-    public static HashSet getExtension(Class extentionClassKey){
+    public static HashSet getExtension(Class extentionClassKey) {
         return extensionMap.get(extentionClassKey);
     }
 
@@ -38,7 +53,7 @@ public class ExtensionClass implements Serializable {
 
     public static void saveExtensionsToFile(String filePath) throws IOException {
         // add some basic security for not getting null pointer exception
-        if(filePath==null) filePath = DEFAULT_FILE_PATH;
+        if (filePath == null) filePath = DEFAULT_FILE_PATH;
         //Create Stream for writing to a file
         FileOutputStream fileOutputStream = new FileOutputStream(new File(filePath));
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
@@ -47,18 +62,21 @@ public class ExtensionClass implements Serializable {
         objectOutputStream.close();
     }
 
-    public static void loadExtensionsFromFile() throws IOException, ClassNotFoundException {
-        loadExtensionsFromFile(DEFAULT_FILE_PATH);
+    public static Boolean loadExtensionsFromFile() throws IOException, ClassNotFoundException {
+        return loadExtensionsFromFile(DEFAULT_FILE_PATH);
     }
 
-    public static void loadExtensionsFromFile(String filePath) throws IOException, ClassNotFoundException {
+    public static Boolean loadExtensionsFromFile(String filePath) throws IOException, ClassNotFoundException {
         // ad some basic security for not getting null pointer exception
-        if(filePath==null)filePath = DEFAULT_FILE_PATH;
+        if (filePath == null) filePath = DEFAULT_FILE_PATH;
 
         //create Stream for reading from a file
-        FileInputStream fi = new FileInputStream(new File(filePath));
+        File dataFile = new File(filePath);
+        if(!dataFile.exists()) return false;
+        FileInputStream fi = new FileInputStream(dataFile);
         ObjectInputStream oi = new ObjectInputStream(fi);
-        extensionMap = (Map<Class, HashSet<Class>>) oi.readObject();
+        return
+                ((extensionMap = (Map<Class, HashSet<Class>>) oi.readObject()) != null ? true : false);
     }
 
 }
