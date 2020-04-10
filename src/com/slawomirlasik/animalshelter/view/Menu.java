aa1101725f17af62924d3384cfdd2d3f1743cf85
Sławomir Lasik <slawomir.lasik@gmail.com>
2020-04-10 21:18:24 +0200

    fixed issue for listing help for ? option

--- src/com/slawomirlasik/animalshelter/view/Menu.java
+++ src/com/slawomirlasik/animalshelter/view/Menu.java
@@ -179,7 +179,7 @@ public class Menu {
                 break;
             }
             case "?": {
-                printHelpMessageForConsoleUsageFromTermial
+                printHelpMessageForConsoleUsageFromTermial();
                 break;
             }
             case "add": {
