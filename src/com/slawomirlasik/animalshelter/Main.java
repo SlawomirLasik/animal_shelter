537623eac63dcfcf7ba76793409b4cc1ca9c71fe
Sławomir Lasik <slawomir.lasik@gmail.com>
2020-04-16 13:23:14 +0200

    Added new images to add in README.MD file

--- src/com/slawomirlasik/animalshelter/Main.java
+++ src/com/slawomirlasik/animalshelter/Main.java
@@ -5,15 +5,16 @@ import com.slawomirlasik.animalshelter.view.Menu;
 
 public class Main {
 
+    private static final Integer MAXIMUM_OF_ANIMALS_IN_THE_SHELTER = 10;
 
     public static void main(String[] args) {
 
         if(args.length > 0){
-            Menu menu = new Menu(new Shelter("Animal Heaven", 1));
+            Menu menu = new Menu(new Shelter("Animal Heaven", MAXIMUM_OF_ANIMALS_IN_THE_SHELTER));
             menu.runCommand(args);
         }
         else{
-            Menu menu = new Menu(new Shelter("Animal Heaven", 1));
+            Menu menu = new Menu(new Shelter("Animal Heaven", MAXIMUM_OF_ANIMALS_IN_THE_SHELTER));
             menu.showMenu();
         }
     }
